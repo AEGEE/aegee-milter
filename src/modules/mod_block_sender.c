3c07fb6df3937f744ac75614b4455dbb926542ba
Дилян Палаузов <git-dpa@aegee.org>
2011-12-04 18:26:04 +0000

    new module: mod_block_sender

---
+++ src/modules/mod_block_sender.c
@@ -0,0 +1,27 @@
+#include "prdr-mod.h"
+#include <time.h>
+
+GKeyFile* prdr_inifile;
+
+int
+mod_block_sender_LTX_prdr_mod_status (void* priv UNUSED)
+{
+  return MOD_RCPT;
+}
+
+int
+mod_block_sender_LTX_prdr_mod_run (void* priv)
+{
+  struct privdata *cont = (struct privdata*) priv;
+  char* sender = prdr_get_envsender(cont);
+  if (strcasecmp(sender, "renekeijzer@mail.com") == 0) {
+    prdr_set_response (priv, "550", "5.7.0", "Rene, your mailbox is hacked and until you resolve the issue AEGEE.org is not going to accept mails from it.\r\n\r\n   Dilyan // AEGEE Mail Team ");
+    prdr_list_insert ("log", sender, "mod_block_sender", "blocked", 0);
+  }
+  return 0;
+}
+
+int
+mod_block_sender_LTX_prdr_mod_equal (struct privdata* priv UNUSED, char* user1 UNUSED, char* user2 UNUSED) {
+  return 1;
+}
